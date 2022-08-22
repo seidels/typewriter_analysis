97c78972c41317ceb01a0881f98aca826b8ef04e
Sophie <seidels@ethz.ch>
2022-08-22 14:13:55 +0200

    Draft for edit outcome exploration Addresses #1

---
+++ src/exploratory/edit_outcomes.R
@@ -0,0 +1,44 @@
+## ---------------------------
+##
+## Script name:
+##
+## Purpose of script:
+##
+## Author: Sophie Seidel & Antoine Zwaans
+##
+## Date Created: 2022-08-22
+##
+## Copyright (c) Sophie Seidel, 2022
+## Email: sophie.seidel@posteo.de
+##
+## ---------------------------
+##
+## Notes:
+##
+##
+## ---------------------------
+
+## set working directory for Mac
+
+setwd("~/Projects/")      # Sophie's working directory (mac)
+
+## ---------------------------
+
+
+## ---------------------------
+
+## load up our functions into memory
+
+# source("functions/summarise_data.R")
+
+## ---------------------------
+
+## Check edit outcome probability site-independent
+
+
+## ---------------------------
+
+## Check edit outcome probability conditional on previous site
+
+
+
