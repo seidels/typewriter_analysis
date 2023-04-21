4affe546b4829bc1102f12de8b25bf8b21c7637e
Sophie <seidels@ethz.ch>
2023-04-21 15:56:26 +0200

    Add scripts that explore prelim gastruloid dat - the number of edits at different targetBCs - the edit outcome distributions at different targetBCs
    for each with all sequences and with unique sequences
    #9

--- src/useful_scripts_across_categories.R
+++ src/useful_scripts_across_categories.R
@@ -1,5 +1,48 @@
 
 
+get_nr_of_edits = function(edit_table, unedited_symbol=NA){
+
+  if(!(is.na(unedited_symbol))){
+    edit_table[edit_table == unedited_symbol] = NA
+  }
+  ## compare to data
+  nr_of_edits = rep(x = 0, nrow(edit_table))
+
+  for (i in 1:nrow(edit_table)){
+
+    if(is.na(edit_table[i, "Site1"])){
+
+      nr_of_edits[i] = 0
+      next
+
+    }else if(is.na(edit_table[i, "Site2"])){
+
+      nr_of_edits[i] = 1
+      next
+
+    }else if (is.na(edit_table[i, "Site3"])){
+
+      nr_of_edits[i] = 2
+      next
+
+    }else if (is.na(edit_table[i, "Site4"])){
+
+      nr_of_edits[i] = 3
+      next
+
+    }else if (is.na(edit_table[i, "Site5"])){
+
+      nr_of_edits[i] = 4
+      next
+
+    }else{
+      nr_of_edits[i] = 5
+    }
+  }
+  return(nr_of_edits)
+}
+
+
 cell_has_selected_barcodes = function(cell, dat, selected_barcodes){
 
   cell_barcodes = dat[dat$Cell == cell, "TargetBC"]
@@ -81,47 +124,4 @@ write_sequence_into_alignment = function(targets_per_cell_dat, targetBC, filenam
 
 write_alignment_closure = function(filename){
   write(x = "</data>", file = filename, append = TRUE, sep = " ")
-}
-
-get_nr_of_edits = function(edit_table, unedited_char){
-
-  if (!(is.na(unedited_char))){
-    edit_table[edit_table == unedited_char] = NA
-  }
-
-  ## compare to data
-  nr_of_edits = rep(x = 0, nrow(edit_table))
-
-  for (i in 1:nrow(edit_table)){
-
-    if(is.na(edit_table[i, "Site1"])){
-
-      nr_of_edits[i] = 0
-      next
-
-    }else if(is.na(edit_table[i, "Site2"])){
-
-      nr_of_edits[i] = 1
-      next
-
-    }else if (is.na(edit_table[i, "Site3"])){
-
-      nr_of_edits[i] = 2
-      next
-
-    }else if (is.na(edit_table[i, "Site4"])){
-
-      nr_of_edits[i] = 3
-      next
-
-    }else if (is.na(edit_table[i, "Site5"])){
-
-      nr_of_edits[i] = 4
-      next
-
-    }else{
-      nr_of_edits[i] = 5
-    }
-  }
-  return(nr_of_edits)
 }
