24b39f5e321b16e870c14754f62110e63fa80cac
azwaans <antoine.zwaans@bsse.ethz.ch>
2023-08-05 21:00:48 +0200

    LTT plots as ggplots

--- src/cell_culture/upgma/plot_LTTs.R
+++ src/cell_culture/upgma/plot_LTTs.R
@@ -1,8 +1,8 @@
 /## ---------------------------
 ##
-## Script name: plot_LTTs
+## Script name: plot_ltts
 ##
-## Purpose of script: plot UPGMA ltt vs BEAST2 outputs
+## Purpose of script: plot UPGMA, MCC LTTs vs BEAST2 outputs
 ##
 ## Author: Antoine Zwaans
 ##
@@ -12,67 +12,55 @@
 ## Email: antoine.zwaans@bsse.ethz.ch
 ##
 
-## set working directory for Mac 
+## set working directory to where trees and log files are
 
-## Set working directory to where the log and tree files are
-setwd("~/typewriter_analysis/results/analysis_cell_culture_data/inference_results/single_clock/1000_cells/")  
+setwd("typewriter_analysis/results/analysis_cell_culture_data/inference_results/single_clock/1000cells/")    
 
 ## load up the packages we will need:  (uncomment as required)
 
 require(tidyverse)
 require(data.table)
-
-## ----------------------
-## Plot MCC against UPGMA
-## ----------------------
-
-#load individual trees
-tree_MCC <- ape::read.nexus(file = "MCC_CommonAncestorHeights.tree")
-tree_UPGMA <- ape::read.tree(file = "~/typewriter_analysis/results/analysis_cell_culture_data/upgma/UPGMAtree_1000.txt")
-
-trees <- c(tree_MCC,tree_UPGMA)
-names(trees) <- c("MCC","UPGMA")
-
-#simple built-in LTT: 
-png(file="LTT_comp.png",
-    width=600, height=600)
-ape::mltt.plot(trees ) 
-dev.off()
+require(ape)
+require(ggplot2)
 
 
 ## ----------------------------------------
 ## Plot UPGMA and MCC against BEAST2 sample
 ## ----------------------------------------
 
-##read in upgma and mcc trees
+##red in SciPhy trees
+trees <- ape::read.nexus(file = "smallest_combined.trees")
+
+##read in upgma and mcc
 upgma <- ape::read.tree(file = "~/typewriter_analysis/results/analysis_cell_culture_data/upgma/UPGMAtree_1000.txt")
 MCC <- ape::read.nexus(file = "MCC_CommonAncestorHeights.tree")
 
-##adjust tip labels
-cell_ids <- read.csv(header = F, file = "~/typewriter_analysis/results/analysis_cell_culture_data/upgma/UPGMAtree_1000_cell_names_.txt")
-cell_ids$numeric_label <- 0:999
-cell_ids_sorted <- cell_ids[match(upgma$tip.label, cell_ids$V1), ]
-upgma$tip.label <- as.character(cell_ids_sorted$numeric_label)
-
-##read in SciPhy trees
-trees <- ape::read.nexus(file = "smallest_combined.trees")
-
-##Plot them all 
-depth.range <- range(unlist(lapply(trees,ape::branching.times)), unlist(lapply(list(upgma),ape::branching.times)))
-max.depth <- sum(abs(depth.range))
-
-png(file="LTT_comp_all_MCC_UPGMA.png",
-    width=30, height=12,units = "cm",res = 300)
-par(mar = c(4.1, 4.1, 1.1, 1.1))
-plot(x=c(0, -1*max.depth), y=c(1, ape::Ntip(trees[[1]])), type="n", bty="n", xaxt="n", xlab="Time (days)", ylab="Number of lineages",cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5)
-colors=c("#E1E1F7",rgb(1,0,0,0.5),"#060647")
-list.of.both <- list(trees, list(upgma),list(MCC))
-for (i in sequence(3)) {
-  tree.list <- list.of.both[[i]]
-  for (j in sequence(length(tree.list))) {
-    ape::ltt.lines(tree.list[[j]], col=colors[[i]])
-  }
+#extract ltt coordinates from all trees
+all_ltt <- c()
+for (i in 1:length(trees)) {
+  coords <- ltt.plot.coords(trees[[i]])
+  all_ltt <- rbind(all_ltt, cbind(coords,number=rep(i,nrow(coords))))
 }
-axis(1, at=seq(-25, 0, by=5), labels = seq(0, 25, by=5), cex.axis=1.5)
-legend(x=-5,y= 200, legend=c("SciPhy", "UPGMA","MCC"), fill=colors)
-dev.off()
+
+#add upgma and mcc coordinates
+ltt_upgma <- ltt.plot.coords(upgma)
+all_ltt <- rbind(all_ltt, cbind(ltt_upgma,rep(length(trees)+1,nrow(ltt_upgma))))
+ltt_mcc <- ltt.plot.coords(MCC)                                
+all_ltt <- rbind(all_ltt, cbind(ltt_mcc,rep(length(trees)+2,nrow(ltt_mcc))))
+all_ltt <- data.frame(all_ltt)
+
+#create another column to colour by type  
+all_ltt$type <- c(rep("SciPhy",length(which(all_ltt$number<=560))),rep("UPGMA",length(which(all_ltt$number==561))),rep("MCC",length(which(all_ltt$number==562))))
+cols <- c("SciPhy" = "#E1E1F7", "MCC" = "#060647", "UPGMA" = rgb(1,0,0,0.5))
+
+ltt_all <-  ggplot(all_ltt,aes(x=time,y=N,group=number,colour=type)) + 
+            geom_step() + 
+            theme_bw() + 
+            scale_color_manual(values = cols) + 
+            theme(legend.position = c(0.8,0.2),legend.title = element_blank()) +
+            ylab("Total lineages") + 
+            xlab("Time (days)")
+
+ggsave("LTT_SciPhy_UPGMA_MCC.png", ltt_all, width = 30, height = 10, units = "cm", dpi = 300)
+
+
