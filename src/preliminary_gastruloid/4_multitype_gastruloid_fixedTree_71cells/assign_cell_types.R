de0751706ca13fe64c67bc0a41a84c0977b7bb71
Sophie <seidels@ethz.ch>
2023-09-16 13:27:10 +0200

    Write type traits output to xml #11

--- src/preliminary_gastruloid/4_multitype_gastruloid_fixedTree_71cells/assign_cell_types.R
+++ src/preliminary_gastruloid/4_multitype_gastruloid_fixedTree_71cells/assign_cell_types.R
@@ -20,18 +20,33 @@
 
 ## set working directory for Mac
 
-setwd("~/Projects/")      # Sophie's working directory (mac)
+setwd("~/Projects/typewriter_analysis/")      # Sophie's working directory (mac)
+source("src/preliminary_gastruloid/scripts_across_gastruloid.R")
 
 ## ---------------------------
+# input cell type annotations
+cell_types = read.csv("results/preliminary_gastruloid/multitype_6types_71cells/annotations.csv",
+                      header = F, col.names = c("cell", "type"))
+cell_types$type_number = sapply(cell_types$type, assign_type_to_number)
 
-## load up the packages we will need:  (uncomment as required)
-
-require(tidyverse)
-
-## ---------------------------
-
-## load up our functions into memory
-
-# source("functions/summarise_data.R")
+# input cell edit file
+integers_dat_file = "results/preliminary_gastruloid/multitype_6types_71cells/edits_to_integer_map.csv"
+integers_dat = read.csv(integers_dat_file)
 
 ## ---------------------------
+# subset cell types to match cells in alignment data; and order according to cell in alignment data
+cell_types = cell_types[cell_types$cell %in% integers_dat$Cell, ]
+cell_types = cell_types[match(unique(integers_dat$Cell), cell_types$cell), ]
+
+## ---------------------------
+# write cell type traits to be inserted into the BEAST 2 xml
+# date trait
+taxon_and_type_individual = unname(apply(cell_types, MARGIN = 1, function(x){
+  paste(x[1], x[3], sep = "=")
+}))
+
+# to be pasted in xml
+taxon_and_type_merged = paste(taxon_and_type_individual, collapse = ",")
+write(taxon_and_type_merged, file = "results/preliminary_gastruloid/multitype_6types_71cells/type_traits.xml")
+
+
