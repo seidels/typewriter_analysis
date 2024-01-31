c100cbc80dd27b0cce6340887191c2ac2d3b1b9b
Sophie <seidels@ethz.ch>
2024-01-31 14:19:21 +0100

    Add validation scripts
    #12

---
+++ src/validations/validations_alignment_per_tree/run.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+
+# draw the clock rate and the insert probabilities from prior distributions
+Rscript draw_simulation_params.R
+
+# run the simulation
+for seed in `seq 2 100`
+do
+    #java -jar 23-04-05-typewriter.jar -seed $seed -D outputDir="/Users/seidels/Projects/typewriter_analysis/results/validations/validations_alignment_per_tree/simulated_values/" simulate_alignment_and_tree.xml
+
+    java -jar 23-04-05-typewriter.jar -overwrite -seed $seed infer_given_fixed_tree_13_inserts.xml
+done
