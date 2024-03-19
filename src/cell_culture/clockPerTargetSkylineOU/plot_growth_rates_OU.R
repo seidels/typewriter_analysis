44f3554ad028d42f63ded7c6da471b6cf90baa7a
azwaans <antoine.zwaans@bsse.ethz.ch>
2024-03-19 11:49:12 +0100

    plot OU growth rates violin plots

--- src/cell_culture/clockPerTargetSkylineOU/plot_growth_rates_OU.R
+++ src/cell_culture/clockPerTargetSkylineOU/plot_growth_rates_OU.R
@@ -31,22 +31,28 @@ typewriter <- read.table(typewriter_file, header = T)
 ##plotting growth rates as violin plots##
 #########################################
 
-birth_rates <- typewriter[,26:28]
-death_rates <- typewriter[,30:32]
+birth_rates <- typewriter[,paste0(rep("birthRate.",13),1:13)]
+death_rates <- typewriter[,paste0(rep("deathRate.",13),1:13)]
 
 growth_rates <- birth_rates - death_rates
 ##add a column with prior
 growth_rates <- bind_cols(growth_rates,Prior=rlnorm(nrow(growth_rates), meanlog = 0.1, sdlog = 1.0) - rlnorm(nrow(growth_rates), meanlog = -0.4, sdlog = 1.0))
-names(growth_rates) <- c("growthRate.1","growthRate.2","growthRate.3","Prior")
+growth_rates <- growth_rates[order(-sapply(growth_rates, median))]
+
+
+
+names(growth_rates) <- c(paste0(rep("growthRate.",13),1:13),"Prior")
 growth_rates_long <- pivot_longer(growth_rates,seq(1,ncol(growth_rates)))
+ordered_names <- c(paste0(rep("growthRate.",13),1:13))
+growth_rates_long <- mutate(growth_rates_long,name = fct_relevel(name,ordered_names,"Prior"))
 
 p <- ggplot(growth_rates_long,aes(x=name,value,fill=name)) +
   geom_violin() + 
   theme(axis.text.x = element_text(angle = 90)) + 
   xlab("Growth") + 
   ylab(parse(text = paste0('"Posterior growth rate "', '(~day^-1)'))) +
-  scale_fill_manual(values=c(rep("#5CA17D",3),"#E1E1F7")) +  
-  coord_cartesian(ylim = c(-0.1,1.2),expand = TRUE) + 
+  scale_fill_manual(values=c(rep("#5CA17D",13),"#E1E1F7")) +  
+  coord_cartesian(ylim = c(-1.0,4.0),expand = TRUE) + 
   theme(legend.position = "none" ,axis.text.x = element_text(angle = 90)) + theme(text=element_text(size = 22),panel.grid.minor = element_blank(),
                                                                                     panel.border = element_blank(),
                                                                                     panel.background = element_blank(),panel.grid.major.x = element_blank())
